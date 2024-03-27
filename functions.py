104703f04d60dd3f1e55ee49347b59509a489246
aprozo <alexandr.a.prozorov@gmail.com>
2024-03-27 09:22:27 +0100

    neural network implement

--- functions.py
+++ functions.py
@@ -3,10 +3,132 @@ import numpy as np
 from tensorflow import keras
 import streamlit as st
 
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
 # Load the model (only executed once!)
 @st.cache_resource
 def load_model():
 	  return keras.models.load_model('model.keras')
+
+all_players = ['Бот','Вадим', 'Ваня', 'Гриша', 'Данил', 'Ден', 'Джун', 'Миша', 'Ондрей', 'Савва', 'Сеньор', 'Юран']
+input_NN_line = [player+"_team1" for player in all_players] + [player+"_team2" for player in all_players]
+
+columnsTeam1 = ['Player1','Player2','Player3','Player4']
+columnsTeam2 = ['Player5','Player6','Player7','Player8']
+columnsPlayers = columnsTeam1 + columnsTeam2
+
+
+map_weights = {
+'ливень':5,
+'кровавая жатва':5, 
+'переход':3,
+'мрачный карнавал':5,
+'холодный ручей':4,
+'похоронный звон':5,
+'приход':5,
+'смерть в воздухе':5,
+'нет милосердию':5,
+'вымерший центр':4,
+'болотная лихорадка':4,
+'последний рубеж':2,
+'жертва':3,
+'роковой полет':5}
+
+def read_data( timeStart='', useMapWeight=False):
+    data=pd.read_csv("datasetNN.csv")
+    # preprocess the data
+    # Date,Map,Player1,Player2,Player3,Player4,Player5,Player6,Player7,Player8,TeamWon
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    if not useMapWeight:
+        data["Map_weight"] =np.ones(len(data)) # remove map weight
+    else:
+        data["Map_weight"] = data["Map"].apply( lambda x: map_weights[x])
+        data["Map_weight"] = data["Map_weight"]/ data["Map_weight"].max()
+    #  drop entries which were played before the start date
+    if timeStart != '':
+        data = data[data['Date'] >= timeStart]
+    
+    data = data[data[columnsPlayers].apply(lambda x: all([player in all_players for player in x]), axis=1)]
+    #  reset the index of data
+    data.reset_index(drop=True, inplace=True)
+
+    #  create a row with all the players and for each player, if he is in team1 or team2, and add TeamWon
+    input_nn = pd.DataFrame(columns=input_NN_line)
+    for i in range(0, len(data)):
+        row = np.zeros(len(input_NN_line))
+        for player in all_players:
+            if player in data.iloc[i][columnsTeam1].values:
+                row[input_NN_line.index(player+"_team1")] = 1
+            if player in data.iloc[i][columnsTeam2].values:
+                row[input_NN_line.index(player+"_team2")] = 1
+        input_nn.loc[len(input_nn)] = row
+
+    input_nn['TeamWon'] = data['TeamWon']
+    if useMapWeight:
+        input_nn['Map_weight'] = data['Map_weight']
+    #  add data with swapped players
+    input_nn_swapped = input_nn.copy()
+    for player in all_players:
+        input_nn_swapped[player+"_team1"], input_nn_swapped[player+"_team2"] = input_nn_swapped[player+"_team2"], input_nn_swapped[player+"_team1"]
+    input_nn_swapped['TeamWon'] = 3 - input_nn_swapped['TeamWon']
+    input_nn = pd.concat([input_nn, input_nn_swapped], ignore_index=True)
+
+    return input_nn
+
+
+
+def train_model(test_size=0.2, nEpochs = 10, useMapWeight=False, timeStart=''):
+
+    input_nn=read_data(timeStart, useMapWeight)
+    # split the data into training and testing
+    input_nn = input_nn.sample(frac=1).reset_index(drop=True)
+    X = input_nn.drop(columns=["TeamWon"])
+    y = input_nn["TeamWon"]-1
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+
+    if useMapWeight: 
+        w_train = X_train['Map_weight']
+        w_test = X_test['Map_weight']
+        X_train = X_train.drop(columns=["Map_weight"])
+        X_test = X_test.drop(columns=["Map_weight"])
+
+    # create the model
+#  create a model
+    model = keras.Sequential([
+    keras.layers.Dense(100, input_shape=(len(input_NN_line),), activation='relu'),
+    keras.layers.Dense(100, activation='relu'), 
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(1, activation='sigmoid')
+    ])
+    from keras.callbacks import EarlyStopping 
+    # Early stopping criteria to avoid overtraining
+    earlystopping = EarlyStopping(patience=10,
+                                verbose=1,
+                                restore_best_weights=True)
+    
+
+    # compile the model
+    model.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics=['accuracy']
+                )
+    
+  
+    # train the model
+    model.fit(X_train,
+            y_train, 
+            batch_size=32,
+            sample_weight=w_train if useMapWeight else None,
+            validation_data=(X_test, y_test), 
+            callbacks=[earlystopping],
+            epochs=nEpochs)
+    
+
+    accuracy = model.evaluate(X_test, y_test)[1]
+
+    return accuracy 
 
 
 def get_teams(present_players):
